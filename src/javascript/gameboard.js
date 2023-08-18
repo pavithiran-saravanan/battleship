eb63abe7aa259b814d10e1814fd9f4d79df21273
pavithiran <pavithransaravanan22@gmail.com>
2023-08-18 15:04:43 +0530

    Create a gameboard factory with functions to place ships on board

---
+++ src/javascript/gameboard.js
@@ -0,0 +1,110 @@
+import getShip from "./ship";
+
+function createCell() {
+  return {
+    isHit: false,
+    hasShip: false,
+    ship: undefined,
+  };
+}
+
+function createBoard() {
+  const board = [];
+  for (let i = 0; i < 10; i += 1) {
+    const row = [];
+    for (let j = 0; j < 10; j += 1) {
+      row.push(createCell());
+    }
+    board.push(row);
+  }
+  return board;
+}
+
+function getShips() {
+  return [getShip(5), getShip(4), getShip(3), getShip(3), getShip(2)];
+}
+
+export default function getGameBoard() {
+  const ships = getShips();
+  const board = createBoard();
+
+  // Returns the name of ship given the Ship Id
+  function getShipName(id) {
+    switch (id) {
+      case 0:
+        return "Carrier";
+      case 1:
+        return "Battleship";
+      case 2:
+        return "Cruiser";
+      case 3:
+        return "Submarine";
+      case 4:
+        return "Destroyer";
+      default:
+        return "Ship";
+    }
+  }
+
+  // Check if given postition lies on board
+  function isOnBoard(row, col) {
+    if (row < 0 || col < 0 || row > 9 || col > 9) return false;
+    return true;
+  }
+
+  // Check if the requested placement is valid
+  function isPlacementValid(row, col, axis, length) {
+    if (axis === "x") {
+      for (let i = col; i < col + length; i += 1) {
+        if (!isOnBoard(row, i) || board[row][i].hasShip) return false;
+      }
+    } else {
+      for (let i = row; i < row + length; i += 1) {
+        if (!isOnBoard(i, col) || board[i][col].hasShip) return false;
+      }
+    }
+    return true;
+  }
+
+  // Place a given ship at specified position along specified direction
+  function placeShip(id, row, col, axis) {
+    if (!isPlacementValid(row, col, axis, ships[id].length)) return false;
+
+    if (axis === "x") {
+      for (let i = col; i < col + ships[id].length; i += 1) {
+        board[row][i].hasShip = true;
+        board[row][i].ship = getShipName(id);
+      }
+    } else {
+      for (let i = row; i < row + ships[id].length; i += 1) {
+        board[i][col].hasShip = true;
+        board[i][col].ship = getShipName(id);
+      }
+    }
+
+    return true;
+  }
+
+  // Place all ships at random positions
+  function randNum() {
+    return Math.floor(Math.random() * 9);
+  }
+
+  function randDir() {
+    return Math.floor(Math.random() * 2) === 1 ? "x" : "y";
+  }
+
+  function placeShipsRandom() {
+    ships.forEach((ship, shipId) => {
+      while (true) {
+        if (placeShip(shipId, randNum(), randNum(), randDir())) break;
+      }
+    });
+  }
+
+  return {
+    board,
+    placeShip,
+    placeShipsRandom,
+  };
+}
