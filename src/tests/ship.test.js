9e0ba902ca775d4730d5b6330b876b7fa5a18499
pavithiran <pavithransaravanan22@gmail.com>
2023-08-18 15:02:27 +0530

    Update test description

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,34 +1,29 @@
 import getShip from "../javascript/ship";
 
-test("Factory returns a ship of provided length", () => {
-  expect(getShip(2).length).toBe(2);
-  expect(getShip(2).hits).toBe(0);
+test("Factory returns a ship that is not sunk", () => {
   expect(getShip(2).isSunk()).toBeFalsy();
 });
 
-test("Ship is hit", () => {
-  const len = 3;
-  const ship = getShip(len);
-  const initialHits = ship.hits;
+test("Ship of len 3 is hit once", () => {
+  const ship = getShip(3);
   ship.hit();
-  expect(ship.hits).toBe(initialHits + 1);
+  expect(ship.isSunk()).toBeFalsy();
 });
 
-test("Is ship sunk", () => {
-  const len = 3;
-  const ship = getShip(len);
+test("Ship of len 3 is hit thrice", () => {
+  const ship = getShip(3);
   ship.hit();
   ship.hit();
   ship.hit();
-  expect(ship.isSunk()).toBe(true);
+  expect(ship.isSunk()).toBeTruthy();
 });
 
-test("Ship is hit after being sunk", () => {
-  const len = 3;
-  const ship = getShip(len);
+test("Ship of len 3 is hit more than thrice", () => {
+  const ship = getShip(3);
   ship.hit();
   ship.hit();
   ship.hit();
   ship.hit();
-  expect(ship.hits).toBe(3);
+  ship.hit();
+  expect(ship.isSunk()).toBeTruthy();
 });
