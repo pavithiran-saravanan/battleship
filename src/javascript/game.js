bbfd4c73e2bc47c25b1018623820d83d03e26b05
pavithiran <pavithransaravanan22@gmail.com>
2023-08-24 21:02:39 +0530

    Extract game logic to separate module

---
+++ src/javascript/game.js
@@ -0,0 +1,62 @@
+import getComputer from "./computer";
+import { renderShips } from "./dom";
+import getPlayer from "./player";
+
+export default function getGame() {
+  // Get Players
+  const player = getPlayer();
+  const computer = getComputer();
+
+  // Board matrices
+  const pBoard = player.gameBoard.board;
+  const cBoard = computer.gameBoard.board;
+
+  // Computer Turn
+  function computerTurn() {
+    const computerBoard = document.querySelector(".computer-board");
+    let target = computer.play();
+    while (!player.gameBoard.receiveAttack(target.row, target.col)) {
+      target = computer.play();
+    }
+    renderShips(pBoard, null);
+    if (player.gameBoard.allShipsSunk()) {
+      console.log("*** GAME OVER. COMPUTER WON ***");
+    } else {
+      computerBoard.style.pointerEvents = "auto";
+      playerTurn();
+    }
+  }
+
+  // Player Turn
+  function playerTurn() {
+    const computerBoard = document.querySelector(".computer-board");
+    const cells = computerBoard.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        const row = cell.getAttribute("data-row");
+        const col = cell.getAttribute("data-col");
+        if (computer.gameBoard.receiveAttack(row, col)) {
+          console.log(row, col);
+          console.log("valid attack");
+          renderShips(null, cBoard);
+          if (computer.gameBoard.allShipsSunk()) {
+            console.log("*** GAME OVER. PLAYER WON ***");
+          } else {
+            setTimeout(computerTurn, 10);
+          }
+          computerBoard.style.pointerEvents = "none";
+        }
+      });
+    });
+  }
+
+  function start() {
+    player.placeShips();
+    computer.placeShips();
+    renderShips(pBoard, cBoard);
+    setTimeout(playerTurn, 500);
+  }
+  return {
+    start
+  };
+}
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,61 +1,7 @@
-import getComputer from "./computer";
-import renderStaticContent, { renderShips } from "./dom";
-import getPlayer from "./player";
+import renderStaticContent from "./dom";
 import "../stylesheet/style.css";
+import getGame from "./game";
 
 renderStaticContent();
-
-// Get Players
-const player = getPlayer();
-const computer = getComputer();
-
-// Get Ships
-player.placeShips();
-computer.placeShips();
-
-// Get board matrices
-const pBoard = player.gameBoard.board;
-const cBoard = computer.gameBoard.board;
-
-// Add boards to dom
-renderShips(pBoard, cBoard);
-
-// Computer's dom board
-const computerBoard = document.querySelector(".computer-board");
-
-function playerTurn() {
-  const cells = computerBoard.querySelectorAll(".cell");
-  cells.forEach((cell) => {
-    cell.addEventListener("click", () => {
-      const row = cell.getAttribute("data-row");
-      const col = cell.getAttribute("data-col");
-      if (computer.gameBoard.receiveAttack(row, col)) {
-        console.log(row, col);
-        console.log("valid attack");
-        renderShips(null, cBoard);
-        if (computer.gameBoard.allShipsSunk()) {
-          console.log("*** GAME OVER. PLAYER WON ***");
-        } else {
-          setTimeout(computerTurn, 500);
-        }
-        computerBoard.style.pointerEvents = "none";
-      }
-    });
-  });
-}
-
-function computerTurn() {
-  let target = computer.play();
-  while (!player.gameBoard.receiveAttack(target.row, target.col)) {
-    target = computer.play();
-  }
-  renderShips(pBoard, null);
-  if (player.gameBoard.allShipsSunk()) {
-    console.log("*** GAME OVER. COMPUTER WON ***");
-  } else {
-    computerBoard.style.pointerEvents = "auto";
-    playerTurn();
-  }
-}
-
-setTimeout(playerTurn, 1000);
+const game = getGame();
+game.start();
